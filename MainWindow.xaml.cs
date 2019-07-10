61e79579edb97718b8298925c61326157968c28e
zfelice <zachfelice@aol.com>
2019-07-10 12:45:19 -0400

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace ParsingCars
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if(searchInput.Text != null)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load("cars.xml");
+
+                foreach(XmlNode node in doc.DocumentElement)
+                {
+                    string name = node.Attributes[0].InnerText;
+                    if(name == searchInput.Text)
+                    {
+                        foreach(XmlNode child in node.ChildNodes)
+                        {
+                            searchResults.Items.Add(child.InnerText);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid Input");
+            }
+        }
+
+    }
+}
